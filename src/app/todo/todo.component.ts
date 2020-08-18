3212ff3a25d258b7cddeadfd26316de1b64cb1a9
Alex <a2weber94@gmail.com>
2020-08-18 12:14:19 -0400

    Lab 2 finished submit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../interfaces/Todo';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -18,7 +19,7 @@ export class TodoComponent implements OnInit {
     },
     {
       task: 'eat breakfast',
-      completed: true,
+      completed: false,
     },
     {
       task: 'look at cat memes',
@@ -26,7 +27,50 @@ export class TodoComponent implements OnInit {
     },
   ];
 
+  searchTerm: string;
+
+  showIndex: number;
+
   constructor() {}
+  //submit new task
+  submitTask(form: NgForm) {
+    let newTask: Todo = {
+      task: form.value.task,
+      completed: false,
+    };
+    this.todos.push(newTask);
+  }
+  //delete item after clicking X button
+  deleteTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+  //Complete task button function
+  completeTask(index: number) {
+    this.todos[index].completed = true;
+  }
+  //Search terms
+  setSearchTerm(form: NgForm) {
+    this.searchTerm = form.value.searchTerm.trim().toLowerCase();
+  }
+  filter() {
+    if (!this.searchTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((items) => {
+        let currentTodo = items.task.toLowerCase().trim();
+        return currentTodo.includes(this.searchTerm);
+      });
+    }
+  }
+  //setShowIndex
+  setShowIndex(index: number) {
+    this.showIndex = index;
+  }
+
+  //make it so you can hit enter for updating tasks
+  resetShowIndex() {
+    this.showIndex = undefined;
+  }
 
   ngOnInit(): void {}
 }
